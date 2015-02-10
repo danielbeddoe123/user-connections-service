ac958181ae507c0fb8ff71a72cfae0381df8baee
beddoed <daniel.beddoe123@googlemail.com>
2015-02-10 21:25:34 +0000

    Added base Spring Boot and Maven files
    The user-connection-service will use Spring Boot to configure and
    launch the service. Maven will be the build tool of choice for the
    project.

---
+++ src/main/java/com.beddoed/UserConnectionsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.beddoed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserConnectionsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserConnectionsServiceApplication.class, args);
+    }
+}
