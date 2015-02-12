48a05b14428110db97c9d4ced3fb07deeaae7bec
beddoed <daniel.beddoe123@googlemail.com>
2015-02-12 21:14:53 +0000

    Modified API to conform to User connection requirements

--- src/main/java/com.beddoed/controller/UserController.java
+++ src/main/java/com.beddoed/controller/UserController.java
@@ -1,15 +1,23 @@
 package com.beddoed.controller;
 
 import com.beddoed.model.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
-    @RequestMapping("/user")
-    public User getUser(@RequestParam(value="name") String name) {
-        return new User("id", name);
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
+    public String getAllUsersConnectedWithUser(@PathVariable String id) {
+        return id;
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public String addUser(@RequestBody User user) {
+        return user.toString();
+    }
+
+    @RequestMapping(value = "/users/{id_1}/connect/{id_2}", method = RequestMethod.PUT)
+    public String addConnectionBetweenUsers(@PathVariable String id_1, @PathVariable String id_2) {
+        return "Connecting: " + id_1 + " with " + id_2;
     }
 }
--- src/main/java/com.beddoed/model/User.java
+++ src/main/java/com.beddoed/model/User.java
@@ -1,13 +1,15 @@
 package com.beddoed.model;
 
 public class User {
-    private final String id;
-    private final String name;
+    private String id;
+    private String name;
 
     public User(String id, String name) {
         this.id = id;
         this.name = name;
     }
+
+    public User() { }
 
     public String getId() {
         return id;
@@ -15,5 +17,9 @@ public class User {
 
     public String getName() {
         return name;
+    }
+
+    public String toString() {
+        return getId() + " - " + getName();
     }
 }
